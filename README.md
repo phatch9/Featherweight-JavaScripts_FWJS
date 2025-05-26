300d54ccfb18195f33f3b39bade53c301a169c80
Phat <phat.chauu26@gmail.com>
2025-05-25 19:13:08 -0700

    initialized new task

--- README.md
+++ README.md
@@ -46,3 +46,82 @@ A Makefile (http://www.gnu.org/software/make/manual/make.html) is provided for t
 `make test` runs the unit tests for this assignment.  All tests should pass once you have completed the assignment.
 
 `make solution.zip` will produce the zip file of your src/ directory 
+
+
+## Write the lexer
+At this point, you have written an interpreter for our Featherweight JavaScript language.
+In this assignment, you will write the lexer and parser for this language using ANTLR.
+(We won't connect the two just yet -- that will be the next assignment).
+
+Before you can write the parsing rules, you will need to specify the lexing rules.  In this step, you specify the valid words of the language.
+
+In ANTLR, lexing rules start with a capital letter.
+
+The reserved words in FWJS are:
+*if
+*else
+*while
+*function
+*var
+*print
+
+Additionally, you will need rules for literals:
+*BOOLs will be either 'true' or 'false'.
+*INTs are integers of any length.  There should be no unnecessary leading zeroes.
+*NULL is represented by the word 'null'.
+
+You will also have various symbols in the language:
+* '*', '/', '+', '-', '%' are the usual mathematical operations.
+* '>', '<', '>=', '<=', '==' are the usual equality comparisons.
+* ';' separates statements.
+
+You will need identifiers.  The rules for variable names in FWJS:
+*The first character must be an alphabetic character or an underscore.
+*The remaining characters must be alphabetic characters, numeric characters, or underscores.
+
+Finally, you will need to consider whitespace and comments.
+Neither should be part of the resulting AST, so be sure to use 'skip'.
+*NEWLINEs are either '\r\n' or '\n'
+*BLOCK_COMMENTs are the usual C-style /* comments */.  They do not support nesting.
+*LINE_COMMENTs comment out everything until the end of the line.  They use the usual C++ style // where everything after a slash-slash is a comment.
+*Spaces and tabs are whitespace characters, and can be ignored.
+
+
+## Write the parser
+----------------------------
+Once you have your lexing rules complete, you may add your parsing rules.
+In ANTLR, parsing rules start with a lower case letter.
+
+The rules are as follows:
+
+A program is one or more statements.
+
+A statement may be:
+*An expression followed by a semicolon.
+*An if statement, with the usual JavaScript syntax.
+*A while statement.
+*A print statement.
+*An empty statement (a semicolon by itself).
+
+An expression can be:
+*Binary operations, where
+...'*', '/', '%' are the highest precedence.
+...'+' and '-' middle precedence.
+...'<', '<=', '>', '>=', and '==' are the lowest precedence.
+*A function declaration (e.g. function(x){ x+1 })
+*A function application (e.g. f(1,2))
+*A variable declaration (var x=3)
+*a variable reference (x)
+*An assignment statement (x=4)
+*A constant (int, bool, or null)
+*An expression in parenthesis
+
+The sampleParseTrees/ directory contains some parse trees for the programs found in fwjsScripts/.  All .tree files were generated with grun's -tree option.  Your parse trees should be similar, but might not be exactly identical.  (Note that grun supports a -gui option that might be helpful to examine).
+
+## Write a visitor
+----------------------------
+Use the same directory structure as in part 2 of the assignment.  Download ExpressionBuilderVisitor.java and Interpreter.java and put them in src/edu/sjsu/fwjs/.
+
+ExpressionBuilderVisitor.java constructs an AST of Expressions (from Expression.java).  Complete this code.  Once you have it working, you can use Interpreter.java (replacing the previous Interpreter.java) to interpret the scripts in the fwjsScripts directory.
+
+The new version of the Makefile contains a 'run' target that will execute all of the fwjs scripts that you have been provided.
---
+++ generatedSrc/edu/sjsu/fwjs/parser/FeatherweightJavaScriptVisitor.java
@@ -0,0 +1,159 @@
+// Generated from FeatherweightJavaScript.g4 by ANTLR 4.13.2
+ package edu.sjsu.fwjs.parser; 
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link FeatherweightJavaScriptParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface FeatherweightJavaScriptVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link FeatherweightJavaScriptParser#prog}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProg(FeatherweightJavaScriptParser.ProgContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code bareExpr}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBareExpr(FeatherweightJavaScriptParser.BareExprContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code ifThenElse}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIfThenElse(FeatherweightJavaScriptParser.IfThenElseContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code ifThen}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIfThen(FeatherweightJavaScriptParser.IfThenContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code while}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitWhile(FeatherweightJavaScriptParser.WhileContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code print}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrint(FeatherweightJavaScriptParser.PrintContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code empty}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEmpty(FeatherweightJavaScriptParser.EmptyContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code varDec}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#stat}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVarDec(FeatherweightJavaScriptParser.VarDecContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code functionDec}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFunctionDec(FeatherweightJavaScriptParser.FunctionDecContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code mulDivMod}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMulDivMod(FeatherweightJavaScriptParser.MulDivModContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code parens}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitParens(FeatherweightJavaScriptParser.ParensContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code functionApp}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFunctionApp(FeatherweightJavaScriptParser.FunctionAppContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code bool}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBool(FeatherweightJavaScriptParser.BoolContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code null}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNull(FeatherweightJavaScriptParser.NullContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code addSub}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAddSub(FeatherweightJavaScriptParser.AddSubContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code id}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitId(FeatherweightJavaScriptParser.IdContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code equality}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEquality(FeatherweightJavaScriptParser.EqualityContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code int}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInt(FeatherweightJavaScriptParser.IntContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code assiState}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssiState(FeatherweightJavaScriptParser.AssiStateContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code fullBlock}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#block}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFullBlock(FeatherweightJavaScriptParser.FullBlockContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code simpBlock}
+	 * labeled alternative in {@link FeatherweightJavaScriptParser#block}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSimpBlock(FeatherweightJavaScriptParser.SimpBlockContext ctx);
+}
